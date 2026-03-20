fa02a6f63a3e5c81ad242122fba1e38d1aaf1d49
kindred77 <109327586@qq.com>
2026-03-20 18:28:06 +0800

    add librime test

--- cmake/deps/BuildLibrime.cmake
+++ cmake/deps/BuildLibrime.cmake
@@ -2,37 +2,112 @@ IF(NOT MIR2X_BUILD_LIBRIME)
     RETURN()
 ENDIF()
 
-MESSAGE(STATUS "Add librime...")
+MESSAGE(STATUS "Begin to build librime...")
 
-INCLUDE(ExternalProject)
+#INCLUDE(C:/mywork/projects/cpp/librime/install/share/cmake/rime/RimeConfig.cmake)
+#SET(Rime_INCLUDE_DIR "C:/mywork/projects/cpp/librime/install/include")
+#SET(Rime_LIBDIR "C:/mywork/projects/cpp/librime/install/lib")
+#SET(Rime_LIB "${CMAKE_STATIC_LIBRARY_PREFIX}rime${CMAKE_STATIC_LIBRARY_SUFFIX}")
+#LINK_DIRECTORIES(${Rime_LIBDIR})
+#MESSAGE(STATUS "------${Rime_INCLUDE_DIR}---------${Rime_LIBDIR}----${Rime_LIB}")
+FIND_PACKAGE(PkgConfig REQUIRED)
+SET(ENV{PKG_CONFIG_PATH} C:/mywork/projects/cpp/librime/install/lib/pkgconfig:$ENV{PKG_CONFIG_PATH})
+MESSAGE(STATUS "Update PKG_CONFIG_PATH: $ENV{PKG_CONFIG_PATH}")
 
-# for the CMAKE_ARGS, the author recommends -DCPACK_PACKAGING_INSTALL_PREFIX=xxx
-# but seems on windows this doesn't work
+RETURN()
+INCLUDE(FetchContent)
 
-ExternalProject_Add(
-        librime
+IF(WIN32)
+    MESSAGE(STATUS "We need to build berkeleydb manually")
+    SET(LIBDB_INCLUDE "C:/mywork/projects/cpp/berkeleydb/build_windows")
+    IF(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        SET(LIBDB_LIBS "C:/mywork/projects/cpp/berkeleydb/build_windows/x64/Debug")
+    ELSEIF (CMAKE_BUILD_TYPE STREQUAL "Release")
+        SET(LIBDB_LIBS "C:/mywork/projects/cpp/berkeleydb/build_windows/x64/Release")
+    ELSE ()
+        MESSAGE(FATAL_ERROR "Not supported build type: ${CMAKE_BUILD_TYPE}")
+    ENDIF ()
+ELSE()
+    FetchContent_Declare(
+        libdb
 
-        GIT_REPOSITORY "https://github.com/rime/librime.git"
-#        GIT_TAG        "master"
-        GIT_CLONE_FLAGS   "--recurse-submodules --timeout=6000"     # 递归子模块 + 浅克隆（加快速度）
-        #PATCH_COMMAND     git submodule update --init --recursive --force
+        GIT_REPOSITORY https://github.com/berkeleydb/libdb.git
+        GIT_TAG        master
 
-        SOURCE_DIR "${MIR2X_3RD_PARTY_DIR}/librime"
-        INSTALL_DIR "${MIR2X_3RD_PARTY_DIR}/librime/build"
+        GIT_PROGRESS 1
+    )
 
-#        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${MIR2X_3RD_PARTY_DIR}/librime/build/install -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC=ON -DBUILD_TEST=OFF
+    FetchContent_MakeAvailable(libdb)
 
-        LOG_DOWNLOAD      1
-        LOG_BUILD 1
-        LOG_CONFIGURE 1
-        LOG_INSTALL 1
+    IF(NOT DEFINED libdb_CONFIGURED)
+        MESSAGE(STATUS "Configuring libdb")
+        EXECUTE_PROCESS(
+            COMMAND ${libdb_SOURCE_DIR}/dist/configure --prefix=${MIR2X_3RD_PARTY_DIR}/libdb/build --enable-shared=no
+            WORKING_DIRECTORY ${libdb_BINARY_DIR}
+
+            RESULT_VARIABLE libdb_CONFIGURED
+
+            OUTPUT_QUIET
+            COMMAND_ERROR_IS_FATAL ANY
+        )
+    ENDIF()
+
+    IF(NOT DEFINED libdb_INSTALLED)
+        MESSAGE(STATUS "Building libdb")
+        EXECUTE_PROCESS(
+            COMMAND make install
+            WORKING_DIRECTORY ${libdb_BINARY_DIR}
+
+            RESULT_VARIABLE libdb_INSTALLED
+
+            OUTPUT_QUIET
+            ERROR_QUIET
+        )
+    ENDIF()
+
+    SET(LIBDB_INCLUDE ${MIR2X_3RD_PARTY_DIR}/libdb/build/include)
+    SET(LIBDB_LIBS ${MIR2X_3RD_PARTY_DIR}/libdb/build/lib)
+ENDIF ()
+
+FetchContent_Declare(
+    libpinyin
+
+    GIT_REPOSITORY https://github.com/kindred77/libpinyin.git
+    GIT_TAG        main
+
+    GIT_PROGRESS 1
 )
 
-SET(RIME_INCLUDE_DIRS "${MIR2X_3RD_PARTY_DIR}/librime/build/install/include")
-SET(RIME_LIBRARIES    "${CMAKE_STATIC_LIBRARY_PREFIX}librime${CMAKE_STATIC_LIBRARY_SUFFIX}")
+SET(DB_FOUND TRUE)
+SET(DB_INCLUDE_DIR ${LIBDB_INCLUDE})
+SET(DB_LIBRARIES ${LIBDB_LIBS})
+FetchContent_MakeAvailable(libpinyin)
 
-INCLUDE_DIRECTORIES(SYSTEM ${RIME_INCLUDE_DIRS})
-LINK_DIRECTORIES(${MIR2X_3RD_PARTY_DIR}/librime/build/install/lib)
+IF(NOT DEFINED libpinyin_CONFIGURED)
+    MESSAGE(STATUS "Configuring libpinyin")
+    EXECUTE_PROCESS(
+        COMMAND env CFLAGS=-I${LIBDB_INCLUDE} CXXFLAGS=-I${LIBDB_INCLUDE} LIBS=-L${LIBDB_LIBS} sh ${libpinyin_SOURCE_DIR}/autogen.sh --prefix=${MIR2X_3RD_PARTY_DIR}/libpinyin/build --enable-shared=no
+        WORKING_DIRECTORY ${libpinyin_SOURCE_DIR}
 
-MESSAGE(STATUS "librime include ${RIME_INCLUDE_DIRS}, lib: ${MIR2X_3RD_PARTY_DIR}/librime/build/install/lib/${RIME_LIBRARIES}")
-#ADD_DEPENDENCIES(mir2x_3rds librime)
+        RESULT_VARIABLE libpinyin_CONFIGURED
+
+        OUTPUT_QUIET
+        COMMAND_ERROR_IS_FATAL ANY
+    )
+ENDIF()
+
+IF(NOT DEFINED libpinyin_INSTALLED)
+    MESSAGE(STATUS "Building libpinyin")
+    EXECUTE_PROCESS(
+        COMMAND make install
+        WORKING_DIRECTORY ${libpinyin_SOURCE_DIR}
+
+        RESULT_VARIABLE libpinyin_INSTALLED
+
+        OUTPUT_QUIET
+        ERROR_QUIET
+    )
+ENDIF()
+
+SET(ENV{PKG_CONFIG_PATH} ${MIR2X_3RD_PARTY_DIR}/libpinyin/build/lib/pkgconfig:$ENV{PKG_CONFIG_PATH})
+MESSAGE(STATUS "Update PKG_CONFIG_PATH: $ENV{PKG_CONFIG_PATH}")
