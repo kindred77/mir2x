a1cbfd6a5848f72d7192ade3efccbf5d757ffb35
kindred77 <109327586@qq.com>
2026-03-22 15:56:27 +0800

    add librime test

--- client/src/ime.cpp
+++ client/src/ime.cpp
@@ -15,6 +15,36 @@
 // for better implementation, check
 // https://github.com/libpinyin/ibus-libpinyin.git
 
+#ifdef USE_LIBRIME
+#include <rime_api.h>
+
+void on_message([[maybe_unused]] void* context_object,
+                RimeSessionId session_id,
+                const char* message_type,
+                const char* message_value) {
+    printf("message: [%zu] [%s] %s\n", session_id, message_type, message_value);
+    RimeApi* rime = rime_get_api();
+    if (RIME_API_AVAILABLE(rime, get_state_label) &&
+        !strcmp(message_type, "option")) {
+        Bool state = message_value[0] != '!';
+        const char* option_name = message_value + !state;
+        const char* state_label =
+            rime->get_state_label(session_id, option_name, state);
+        if (state_label) {
+            printf("updated option: %s = %d // %s\n", option_name, state,
+                   state_label);
+        }
+        }
+}
+
+RimeSessionId ensure_session(RimeApi* rime) {
+    RimeSessionId id = rime->create_session();
+    if (!id) {
+        fprintf(stderr, "Error creating rime session.\n");
+    }
+    return id;
+}
+
 struct _IME_Instance final
 {
     std::string input;
@@ -30,31 +60,29 @@ struct _IME_Instance final
     mutable std::mutex mtx;
     mutable std::condition_variable cond;
 
-    //pinyin_context_t *context;
-    //pinyin_instance_t *instance;
+//    pinyin_context_t *context;
+//    pinyin_instance_t *instance;
 
     _IME_Instance()
     {
-        // suppress warning message: open libpinyin/conf/user.conf failed.
-        // create libpinyin/data/user.conf if not exists
+        RimeApi* rime = rime_get_api();
 
-        //if(const std::filesystem::path conf = "libpinyin/conf/user.conf"; !std::filesystem::exists(conf)){
-        //    std::filesystem::create_directories(conf.parent_path());
-        //    std::ofstream{conf};
-        //}
+        RIME_STRUCT(RimeTraits, traits);
+        traits.app_name = "rime.console";
+        rime->setup(&traits);
+        rime->set_notification_handler(&on_message, NULL);
+        rime->initialize(NULL);
+        Bool full_check = True;
+        if (rime->start_maintenance(full_check))
+            rime->join_maintenance_thread();
 
-        //context = pinyin_init("libpinyin/data", "libpinyin/conf");
-        //fflassert(context);
+        RimeSessionId session_id = 0;
+        if (!rime->find_session(session_id) &&
+            !(session_id = ensure_session(rime))) {
+            fprintf(stderr, "can not get session.\n");
+        }
 
-        //pinyin_set_options(context,
-        //        static_cast<guint32>(PINYIN_INCOMPLETE ) |
-        //         static_cast<guint32>(PINYIN_CORRECT_ALL) |
-        //         static_cast<guint32>(USE_DIVIDED_TABLE ) |
-        //         static_cast<guint32>(USE_RESPLIT_TABLE ) |
-        //         static_cast<guint32>(DYNAMIC_ADJUST    ));
-        //
-        // instance = pinyin_alloc_instance(context);
-        // fflassert(instance);
+        fflassert(session_id);
 
         done = false;
         th = std::thread([this]()
@@ -73,50 +101,49 @@ struct _IME_Instance final
                     selection .reset();
                     candidates.clear();
                     stk       .clear();
-
-                    //pinyin_reset(instance);
                     continue;
                 }
 
-                if(selection.has_value()){
-                    //uint num = 0;
-                    //pinyin_get_n_candidate(instance, &num);
-
-                    //if(selection.value() >= num){
-                    //    selection.reset();
-                    //    continue;
-                    //}
-
-                    //const auto choice = selection.value();
-                    selection.reset();
-
-                    //lookup_candidate_t *candidate = nullptr;
-                    //pinyin_get_candidate(instance, choice, &candidate);
-
-                    //const char *word = nullptr;
-                    //pinyin_get_candidate_string(instance, candidate, &word);
-
-                    //lookup_candidate_type_t type;
-                    //pinyin_get_candidate_type(instance, candidate, &type);
-
-                    // const auto [sentence, offset] = [type, word, this]() -> std::pair<std::string, size_t>
-                    // {
-                    //     if((type == NBEST_MATCH_CANDIDATE) || stk.empty()){
-                    //         return {word, 0};
-                    //     }
-                    //     else{
-                    //         return {stk.back().first + word, to_uz(stk.back().second)};
-                    //     }
-                    // }();
-                    //
-                    // stk.emplace_back(sentence, pinyin_choose_candidate(instance, offset, candidate));
-                }
-                else if(stk.empty()){
-                    //pinyin_parse_more_full_pinyins(instance, input.c_str());
-                    //pinyin_guess_sentence_with_prefix(instance, prefix.c_str());
-                }
-
-                candidates.clear();
+                // if(selection.has_value()){
+                //     uint num = 0;
+                //     pinyin_get_n_candidate(instance, &num);
+                //
+                //     if (selection.value() >= num)
+                //     {
+                //         selection.reset();
+                //         continue;
+                //     }
+                //
+                //     const auto choice = selection.value();
+                //     selection.reset();
+                //
+                //     lookup_candidate_t *candidate = nullptr;
+                //     pinyin_get_candidate(instance, choice, &candidate);
+                //
+                //     const char *word = nullptr;
+                //     pinyin_get_candidate_string(instance, candidate, &word);
+                //
+                //     lookup_candidate_type_t type;
+                //     pinyin_get_candidate_type(instance, candidate, &type);
+                //
+                //      const auto [sentence, offset] = [type, word, this]() -> std::pair<std::string, size_t>
+                //      {
+                //          if((type == NBEST_MATCH_CANDIDATE) || stk.empty()){
+                //              return {word, 0};
+                //          }
+                //          else{
+                //              return {stk.back().first + word, to_uz(stk.back().second)};
+                //          }
+                //      }();
+                //
+                //      stk.emplace_back(sentence, pinyin_choose_candidate(instance, offset, candidate));
+                // }
+                // else if(stk.empty()){
+                //     pinyin_parse_more_full_pinyins(instance, input.c_str());
+                //     pinyin_guess_sentence_with_prefix(instance, prefix.c_str());
+                // }
+                //
+                // candidates.clear();
                 // pinyin_guess_candidates(instance, stk.empty() ? 0 : stk.back().second, SORT_BY_PHRASE_LENGTH_AND_PINYIN_LENGTH_AND_FREQUENCY);
                 //
                 // guint num = 0;
@@ -145,11 +172,6 @@ struct _IME_Instance final
 
         th.join();
 
-        // pinyin_free_instance(instance);
-        // pinyin_mask_out(context, 0x0, 0x0);
-        //
-        // pinyin_save(context);
-        // pinyin_fini(context);
     }
 
     // return true if input pinyin string is complete, i.e.
@@ -160,20 +182,173 @@ struct _IME_Instance final
     // we skip the training and saving if input is incomplete
     bool is_input_complete_pinyin()
     {
-        //size_t i = 0;
-        // size_t length = pinyin_get_parsed_input_length(instance);
-        //
-        // for(; i < length; ++i){
-        //     ChewingKey *key = nullptr;
-        //     if(pinyin_get_pinyin_key(instance, i, &key) && key){
-        //         if(pinyin_get_pinyin_is_incomplete(instance, key)){
-        //             return false;
-        //         }
-        //     }
-        // }
+        size_t i = 0;
+
         return true;
     }
 };
+#else
+struct _IME_Instance final
+{
+    std::string input;
+    std::string prefix;
+
+    std::optional<size_t> selection;
+    std::vector<std::string> candidates;
+    std::vector<std::pair<std::string, int>> stk; // (sentence, start)
+
+    bool done;
+    std::thread th;
+
+    mutable std::mutex mtx;
+    mutable std::condition_variable cond;
+
+    pinyin_context_t *context;
+    pinyin_instance_t *instance;
+
+    _IME_Instance()
+    {
+        // suppress warning message: open libpinyin/conf/user.conf failed.
+        // create libpinyin/data/user.conf if not exists
+
+        if(const std::filesystem::path conf = "libpinyin/conf/user.conf"; !std::filesystem::exists(conf)){
+            std::filesystem::create_directories(conf.parent_path());
+            std::ofstream{conf};
+        }
+
+        context = pinyin_init("libpinyin/data", "libpinyin/conf");
+        fflassert(context);
+
+        pinyin_set_options(context,
+                static_cast<guint32>(PINYIN_INCOMPLETE ) |
+                 static_cast<guint32>(PINYIN_CORRECT_ALL) |
+                 static_cast<guint32>(USE_DIVIDED_TABLE ) |
+                 static_cast<guint32>(USE_RESPLIT_TABLE ) |
+                 static_cast<guint32>(DYNAMIC_ADJUST    ));
+
+         instance = pinyin_alloc_instance(context);
+         fflassert(instance);
+
+        done = false;
+        th = std::thread([this]()
+        {
+            while(!done){
+                std::unique_lock<std::mutex> lock(mtx);
+                cond.wait(lock);
+
+                if(done){
+                    return;
+                }
+
+                if(input.empty()){
+                    prefix.clear();
+
+                    selection .reset();
+                    candidates.clear();
+                    stk       .clear();
+
+                    pinyin_reset(instance);
+                    continue;
+                }
+
+                if(selection.has_value()){
+                    uint num = 0;
+                    pinyin_get_n_candidate(instance, &num);
+
+                    if (selection.value() >= num)
+                    {
+                        selection.reset();
+                        continue;
+                    }
+
+                    const auto choice = selection.value();
+                    selection.reset();
+
+                    lookup_candidate_t *candidate = nullptr;
+                    pinyin_get_candidate(instance, choice, &candidate);
+
+                    const char *word = nullptr;
+                    pinyin_get_candidate_string(instance, candidate, &word);
+
+                    lookup_candidate_type_t type;
+                    pinyin_get_candidate_type(instance, candidate, &type);
+
+                     const auto [sentence, offset] = [type, word, this]() -> std::pair<std::string, size_t>
+                     {
+                         if((type == NBEST_MATCH_CANDIDATE) || stk.empty()){
+                             return {word, 0};
+                         }
+                         else{
+                             return {stk.back().first + word, to_uz(stk.back().second)};
+                         }
+                     }();
+
+                     stk.emplace_back(sentence, pinyin_choose_candidate(instance, offset, candidate));
+                }
+                else if(stk.empty()){
+                    pinyin_parse_more_full_pinyins(instance, input.c_str());
+                    pinyin_guess_sentence_with_prefix(instance, prefix.c_str());
+                }
+
+                candidates.clear();
+                pinyin_guess_candidates(instance, stk.empty() ? 0 : stk.back().second, SORT_BY_PHRASE_LENGTH_AND_PINYIN_LENGTH_AND_FREQUENCY);
+
+                guint num = 0;
+                pinyin_get_n_candidate(instance, &num);
+
+                for(guint i = 0; i < num; ++i){
+                    lookup_candidate_t *candidate = nullptr;
+                    pinyin_get_candidate(instance, i, &candidate);
+
+                    const char *word = nullptr;
+                    pinyin_get_candidate_string(instance, candidate, &word);
+
+                    candidates.emplace_back(word);
+                }
+            }
+        });
+    }
+
+    ~_IME_Instance()
+    {
+        {
+            const std::lock_guard<std::mutex> lock(mtx);
+            done = true;
+        }
+        cond.notify_one();
+
+        th.join();
+
+        pinyin_free_instance(instance);
+        pinyin_mask_out(context, 0x0, 0x0);
+
+        pinyin_save(context);
+        pinyin_fini(context);
+    }
+
+    // return true if input pinyin string is complete, i.e.
+    //
+    //   complete: nihaoya
+    // incomplete: nihaoy, nhya
+    //
+    // we skip the training and saving if input is incomplete
+    bool is_input_complete_pinyin()
+    {
+        size_t i = 0;
+         size_t length = pinyin_get_parsed_input_length(instance);
+
+         for(; i < length; ++i){
+             ChewingKey *key = nullptr;
+             if(pinyin_get_pinyin_key(instance, i, &key) && key){
+                 if(pinyin_get_pinyin_is_incomplete(instance, key)){
+                     return false;
+                 }
+             }
+         }
+        return true;
+    }
+};
+#endif
 
 static _IME_Instance * asIMEPtr(void *ptr)
 {
